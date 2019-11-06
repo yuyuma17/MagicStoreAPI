34f6da8685215fb474b6cfdfd8b21e7386c74b1f
Lacie <dark830917@gmail.com>
2019-11-06 18:22:03 +0800

    add VC and homeView ok

---
+++ MagicStoreMix/MyOwnViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyOwnViewController.swift
+//  MagicStoreMix
+//
+//  Created by 黃士軒 on 2019/11/6.
+//  Copyright © 2019 Jes Yang. All rights reserved.
+//
+
+import UIKit
+
+class MyOwnViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
