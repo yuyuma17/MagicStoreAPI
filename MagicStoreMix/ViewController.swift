76f844f3fd6b58097ac639bbbaffac37babd2323
Jes <ilamtibr@gmail.com>
2019-11-06 17:08:59 +0800

    Initial Commit

---
+++ MagicStoreMix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicStoreMix
+//
+//  Created by Jes Yang on 2019/11/6.
+//  Copyright © 2019 Jes Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
