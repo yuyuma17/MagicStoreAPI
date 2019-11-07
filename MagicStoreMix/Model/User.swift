5fba9902bd5b35a5b2e4580cc211b6bf3097e408
Jes <ilamtibr@gmail.com>
2019-11-07 14:48:34 +0800

    Modified user model

--- MagicStoreMix/Model/User.swift
+++ MagicStoreMix/Model/User.swift
@@ -8,9 +8,39 @@
 
 import Foundation
 
-struct User {
+struct User: Codable {
     var name: String
     var totalMoney: Int
     var purchased: [MagicBook]
 }
 
+class UserData {
+    var user = User(name: "Jes", totalMoney: 2000, purchased: [MagicBook]())
+}
+
+class UserPersist {
+        
+    static func saveData(user: User) {
+        // Use PropertyListEncoder to convert Player into Data / NSData
+        do {
+            let userData = try PropertyListEncoder().encode(user)
+            UserDefaults.standard.set(userData, forKey: "userKey")
+        } catch {
+            print("Save data error.")
+        }
+    }
+    
+    static func loadData() -> User? {
+        guard let userData = UserDefaults.standard.object(forKey: "userKey") as? Data else {
+            return nil
+        }
+        
+        // Use PropertyListDecoder to convert Data into Player
+        guard let user = (try? PropertyListDecoder().decode(User.self, from: userData)) else {
+            print("Load data error.")
+            return nil
+        }
+        
+        return user
+    }
+}
