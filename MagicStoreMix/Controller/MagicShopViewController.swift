28814ddee95c652035cca95554b945308fa418ed
ABlackTaco <ablacktaco@gmail.com>
2019-11-07 18:08:34 +0800

    afraid

--- MagicStoreMix/Controller/MagicShopViewController.swift
+++ MagicStoreMix/Controller/MagicShopViewController.swift
@@ -11,22 +11,39 @@ import UIKit
 class MagicShopViewController: UIViewController {
 
     @IBOutlet var changeStateButton: [UIButton]!
-    
-    
+    @IBOutlet var moneyView: UIView!
+    @IBOutlet var userMoney: UILabel!
+    @IBOutlet var magicShopCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        settingStateButtonProperties()
+        setDefaultProperties()
         navigationController?.navigationBar.tintColor = .white
         
     }
     
-    fileprivate func settingStateButtonProperties() {
+    fileprivate func setDefaultProperties() {
         for button in changeStateButton {
             button.contentEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
-            setProperties(button: button, configSetting: .stateButton)
+            setButtonProperties(button: button, configSetting: .stateButton)
         }
+        moneyView.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        moneyView.layer.borderWidth = 2
+        
     }
+    
+}
 
+extension MagicShopViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
 }
---
+++ MagicStoreMix/Model/BorderProperties.swift
@@ -0,0 +1,34 @@
+//
+//  SettingButtonProperties.swift
+//  MagicStoreMix
+//
+//  Created by 陳姿穎 on 2019/11/7.
+//  Copyright © 2019 Jes Yang. All rights reserved.
+//
+
+import UIKit
+
+struct BorderProperties {
+    
+    static let homePageButton = BorderProperties(cornerRadius: 10, borderWidth: 2.0)
+    static let purchaseButton = BorderProperties(cornerRadius: 10, borderWidth: 0.5)
+    static let viewBorder = BorderProperties(borderWidth: 2.0)
+    
+    var cornerRadius: Double = 0.0
+    var bordercolor: CGColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    var borderWidth: Double = 0.0
+    
+}
+
+// DI
+func setButtonBorder(button: UIButton, configSetting: BorderProperties) {
+    button.layer.cornerRadius = CGFloat(configSetting.cornerRadius)
+    button.layer.borderColor = configSetting.bordercolor
+    button.layer.borderWidth = CGFloat(configSetting.borderWidth)
+    button.clipsToBounds = true
+}
+
+func setViewBorder(view: UIView, configSetting: BorderProperties) {
+    view.layer.borderColor = configSetting.bordercolor
+    view.layer.borderWidth = CGFloat(configSetting.borderWidth)
+}
